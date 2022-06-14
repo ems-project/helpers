6fc3c293cac7d500d8eca9bb3e27136afa2d753a
mdk <mathieu.dekeyzer@smals.be>
2022-06-14 15:30:54 +0200

    fix: disable wrap in pretty print

--- src/Html/Html.php
+++ src/Html/Html.php
@@ -14,6 +14,7 @@ class Html
             'indent' => true,
             'indent-spaces' => 2,
             'newline' => 'LF',
+            'wrap' => 0,
         ]);
 
         return \str_replace(["<body>\n  ", "\n</body>", "\n  ", '<body>'], ['', '', "\n", ''], $formatter->body()->value);
